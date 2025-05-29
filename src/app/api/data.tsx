78c51fa1cdef9e5658d74d5047f3855f35518b01
Kamran Kamranifard <kamran.kamranifard@riverty.com>
2025-05-29 12:16:59 +0200

    project builded

---
+++ src/app/api/data.tsx
@@ -0,0 +1,87 @@
+export const FeaturesData: {
+  imgSrc: string;
+  heading: string;
+  subheading: string;
+}[] = [
+  {
+    imgSrc: "/images/features/featureOne.png",
+    heading: "Menu variations",
+    subheading: "Sed ut perspiciatis unde omnis iste natus error",
+  },
+  {
+    imgSrc: "/images/features/featureTwo.png",
+    heading: "Cooking warw",
+    subheading: "Sed ut perspiciatis unde omnis iste natus error",
+  },
+  {
+    imgSrc: "/images/features/featureThree.png",
+    heading: "Best chef",
+    subheading: "Sed ut perspiciatis unde omnis iste natus error",
+  },
+  {
+    imgSrc: "/images/features/featureFour.png",
+    heading: "Fast food",
+    subheading: "Sed ut perspiciatis unde omnis iste natus error",
+  },
+];
+
+export const ExpertData: {
+  profession: string;
+  name: string;
+  imgSrc: string;
+}[] = [
+  {
+    profession: "Senior Chef",
+    name: "Shoo Thar Mien",
+    imgSrc: "/images/Expert/boyone.svg",
+  },
+  {
+    profession: "Junior Chef",
+    name: "Shoo Thar Mien",
+    imgSrc: "/images/Expert/girl.png",
+  },
+  {
+    profession: "Junior Chef",
+    name: "Shoo Thar Mien",
+    imgSrc: "/images/Expert/boytwo.svg",
+  },
+  {
+    profession: "Junior Chef",
+    name: "Shoo Thar Mien",
+    imgSrc: "/images/Expert/girl.png",
+  },
+  {
+    profession: "Senior Chef",
+    name: "Shoo Thar Mien",
+    imgSrc: "/images/Expert/boyone.svg",
+  },
+  {
+    profession: "Junior Chef",
+    name: "Shoo Thar Mien",
+    imgSrc: "/images/Expert/boytwo.svg",
+  },
+];
+
+// data.tsx
+export const galleryImages = [
+  {
+    src: "/images/Gallery/foodone.jpg",
+    name: "Caesar Salad(187 Kcal)",
+    price: 35,
+  },
+  {
+    src: "/images/Gallery/foodtwo.jpg",
+    name: "Christmas salad(118 Kcal)",
+    price: 17,
+  },
+  {
+    src: "/images/Gallery/foodthree.jpg",
+    name: "Sauteed mushrooms with pumpkin and sweet pepper(238 kcal)",
+    price: 45,
+  },
+  {
+    src: "/images/Gallery/foodfour.jpg",
+    name: "BBQ Chicken Feast Pizza(272 kcal)",
+    price: 27,
+  },
+];
